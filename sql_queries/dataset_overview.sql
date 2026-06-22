59fa28b557a38dd92043764217179dfa619d597c
Ayan Goswami <ayan.goswami11799@gmail.com>
2026-06-22 12:53:03 +0530

    Add dataset overview SQL queries
    Added SQL queries for dataset overview including total records, gender distribution, stroke distribution, smoking status, residence type, work type, average age, and average glucose level.

--- sql_queries/dataset_overview.sql
+++ sql_queries/dataset_overview.sql
@@ -1,1 +1,57 @@
+-- ==========================================
+-- DATASET OVERVIEW
+-- Stroke Prediction Dataset
+-- ==========================================
+
+-- Total Number of Records
+SELECT COUNT(*) AS total_patients
+FROM stroke_data;
+
+-- Gender Distribution
+SELECT
+gender,
+COUNT(*) AS patients
+FROM stroke_data
+GROUP BY gender
+ORDER BY patients DESC;
+
+-- Stroke Distribution
+SELECT
+stroke,
+COUNT(*) AS patients
+FROM stroke_data
+GROUP BY stroke;
+
+-- Smoking Status Distribution
+SELECT
+smoking_status,
+COUNT(*) AS patients
+FROM stroke_data
+GROUP BY smoking_status
+ORDER BY patients DESC;
+
+-- Residence Type Distribution
+SELECT
+residence_type,
+COUNT(*) AS patients
+FROM stroke_data
+GROUP BY residence_type;
+
+-- Work Type Distribution
+SELECT
+work_type,
+COUNT(*) AS patients
+FROM stroke_data
+GROUP BY work_type
+ORDER BY patients DESC;
+
+-- Average Age of All Patients
+SELECT
+ROUND(AVG(age),2) AS average_age
+FROM stroke_data;
+
+-- Average Glucose Level
+SELECT
+ROUND(AVG(avg_glucose_level),2) AS average_glucose_level
+FROM stroke_data;
 
